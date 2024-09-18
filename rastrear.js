c7d74d66a85b8dc09f7007b5438f8e87f710163e
Allan Souza <allan46884806@edu.pa.senac.br>
2024-09-18 11:19:08 -0300

    Pesquisar Longitude e Latitude

--- rastrear.js
+++ rastrear.js
@@ -1,5 +1,10 @@
+
+/*var lat = document.getElementById('latitude');
+var long = document.getElementById('longitude');
+var button = document.getElementById('botao');*/
+
 function success(pos){
-    console.log(pos.coords.latitude, pos.coords.longitude);
+    //console.log(pos.coords.latitude, pos.coords.longitude);
 
     // centralizar mapa
     var map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 20);
@@ -17,19 +22,22 @@ function success(pos){
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+    L.Routing.control({
+        waypoints: [
+        L.latLng(pos.coords.latitude, pos.coords.longitude),
+        L.latLng(-1.4560646164723035, -48.501267232480984)
+        ]
+    }).addTo(map);
+
     // criar e apontar icone do local
     var marker = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map);
     marker.bindPopup("<p>Você está aqui</p>").openPopup();
 
-    L.Routing.control({
-        waypoints: [
-          L.latLng(pos.coords.latitude, pos.coords.longitude),
-          L.latLng(-1.4560646164723035, -48.501267232480984)
-        ]
-      }).addTo(map);
+   
 }
 
 
 
-
 navigator.geolocation.getCurrentPosition(success);
+
+
