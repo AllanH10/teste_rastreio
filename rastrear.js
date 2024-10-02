7ffe17fecdc1a28a8a98f0f5911d44e6173d2a94
Allan Souza <allan46884806@edu.pa.senac.br>
2024-10-02 11:03:41 -0300

    Localizando a rota com set interval

--- rastrear.js
+++ rastrear.js
@@ -1,36 +1,86 @@
 if ('geolocation' in navigator) {
-    let map = L.map('map').setView([0, 0], 2); // Inicializa o mapa em uma posição padrão
-    let marker = L.marker([0, 0]).addTo(map) // Cria um marcador inicialmente com uma posição padrão
-        .bindPopup("<p>Você está aqui</p>")
-        .openPopup();
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    
 
     navigator.geolocation.watchPosition(function (pos) {
         const lat = pos.coords.latitude;
         const lon = pos.coords.longitude;
 
+        let map = L.map('map').setView([0, 0], 19); // Inicializa o mapa em uma posição padrão
+        let marker = L.marker([0, 0]).addTo(map) // Cria um marcador inicialmente com uma posição padrão
+            .bindPopup("<p>Você está aqui</p>")
+            .openPopup();
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
         // Atualiza a vista do mapa para a nova posição
-        map.setView([lat, lon], 20);
+       // map.setView([lat, lon], 20);
 
         // Atualiza a posição do marcador existente
-        marker.setLatLng([lat, lon]);
+        //marker.setLatLng([lat, lon]);
 
         // Criar rota (opcional, se precisar)
-        L.Routing.control({
+        controleRota = L.Routing.control({
             waypoints: [
                 L.latLng(lat, lon),
                 L.latLng(-1.4560646164723035, -48.501267232480984)
             ]
         }).addTo(map);
 
+        /*L.Routing.control({
+            waypoints: [
+            L.latLng(pos.coords.latitude, pos.coords.longitude),
+            L.latLng(-1.4560646164723035, -48.501267232480984)
+            ]
+        }).addTo(map);*/
+
+        
+
+
+
+        //var newLat = controleRota.options.waypoints[0].lat+0.01;
+        //var newLng = controleRota.options.waypoints[0].lng+0.01;
+        /*setTimeout(function () {
+        controleRota.options.waypoints=[
+            L.latLng(newLat, newLng),
+            controleRota.options.waypoints[1]
+        ];
+        }, 10000);*/
+
+        setInterval(function () {
+            var newWaypoint = controleRota.getWaypoints()[0].latLng;
+            var newLat = newWaypoint.lat;
+            var newLng = newWaypoint.lng;
+            controleRota.setWaypoints([
+               L.latLng(newLat, newLng),
+               controleRota.options.waypoints[1]
+             ]);
+        }, 10000);
+
+
+
     }, function () {
         alert("Não foi possível obter sua localização");
-        map.setView([0, 0], 19);
+        //map.setView([0, 0], 19);
     });
 } else {
     alert("Geolocalização não é suportada pelo seu navegador");
 }
+
+
+/*routingControl = L.Routing.control({
+    waypoints: [
+        L.latLng(43.12, 11.99),
+        L.latLng(43.37, 12.08)
+    ]
+    /*createMarker: function() { return null; },
+    routeWhileDragging: false,
+    draggableWaypoints: false,
+    reverseWaypoints: false,
+    fitSelectedRoutes: true,
+    addWaypoints: false*/
+//}).addTo(OSM_Map);//
+
+
